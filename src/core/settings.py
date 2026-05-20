535de4422d1ef97257e5b744c956a4669d0b330a
krishnardesai7-ship-it <krishnardesai7@gmail.com>
2026-05-20 12:23:55 +0530

    feat: configure core settings and add reusable message component template

--- src/core/settings.py
+++ src/core/settings.py
@@ -161,8 +161,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'krishnardesai7@gmail.com'
-EMAIL_HOST_PASSWORD = 'slqa xfjx eltx ovtz'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='krishnardesai7@gmail.com')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='slqa xfjx eltx ovtz')
 
 
 # Celery settings
